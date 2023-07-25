604541ff2a818f3da39f0c6183b3035c280701b4
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-07-24 22:04:38 -0400

    working as expected, all routes and functions working

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const noteData = require('./db/db.json');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
@@ -15,7 +14,7 @@ app.use(express.static('public'));
 
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    err ? console.error(err) : console.info(`Data written to ${destination}`)
   );
 
 const readAndAppend = (newNote, file) => {
@@ -25,12 +24,21 @@ const readAndAppend = (newNote, file) => {
     } else {
       const notes = JSON.parse(data);
       notes.push(newNote);
-      //notes.forEach((note, index) => {
-      //  note.id = index + 1;
-      //};
       writeToFile(file, notes);
     }
-  }
+  });
+};
+
+const readAndFilter = (removeNote, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+  const noteData = JSON.parse(data);
+  const newNoteData = [];
+    for (let i = 0; i < noteData.length; i++) {
+      if (removeNote !== noteData[i].id) 
+       newNoteData.push(noteData[i]);
+      }  
+  writeToFile(file, newNoteData);
+}
 )};
 
 //HTML routes
@@ -44,12 +52,15 @@ app.get('/notes', (req, res) =>
 );
 
 // API Routes
-app.get('/api/notes', (req, res) => { res.json(noteData) }
-);
+app.get('/api/notes', (req, res) => { 
+   // this route sends all notes to the page in a json format
+    res.sendFile(path.join(__dirname, 'db/db.json'))
+});
 
 // returns a specific note
 
-app.get('/api/notes/:title', (req, res) => {
+app.get('/api/notes/:id', (req, res) => {
+  const noteData = require('./db/db.json');
   console.log(req.params)
   const requestedId = req.params.id
   for (let i = 0; i < noteData.length; i++) {
@@ -61,31 +72,36 @@ app.get('/api/notes/:title', (req, res) => {
   return res.json('no matching note found');
 });
 
+//Deletes a note
+app.delete('/api/notes/:id', (req, res) => {
+  readAndFilter(req.params.id, './db/db.json');
+  res.json({message: "item deleted successfully"});
+});
+
 // post a new note
 app.post('/api/notes', (req,res) => {
 
  console.log(`${req.method} request received to add a note`);
  
  //deconstruct to create the request body
- const { title, text} = req.body;
+ const { title, text } = req.body;
 
   if (title && text) {
     const newNote = {
       title,
       text,
-      noteId: uuid(),
+      id: uuid(),
     };
-
+    
     readAndAppend(newNote, 'db/db.json');
-   
+
     const response = {
       status: 'success',
       body: newNote,
     };
-
-    res.json(response);
-  } else {
-    res.error('Error adding note');
+    res.status(201).json(response); 
+    } else {
+    res.status(500).json('Error adding note');
   }
 });
 
